7fd5692109cab85c045ba7dd0db180e5f907f9ba
LeTuanAnh210 <letuananh21007@gmail.com>
2025-10-06 16:00:26 +0700

    setup2

--- src/main/java/org/example/baikt1_letuananh_th04288/BaiKt1LeTuanAnhTh04288Application.java
+++ src/main/java/org/example/baikt1_letuananh_th04288/BaiKt1LeTuanAnhTh04288Application.java
@@ -8,6 +8,7 @@ public class BaiKt1LeTuanAnhTh04288Application {
 
     public static void main(String[] args) {
         SpringApplication.run(BaiKt1LeTuanAnhTh04288Application.class, args);
+        System.out.println("Running.....");
     }
 
 }
