f4dd6e4e0db10d6d62aa334bc3bf2cf2151341df
LeTuanAnh210 <letuananh21007@gmail.com>
2025-10-06 15:50:27 +0700

    setup1

---
+++ src/main/java/org/example/baikt1_letuananh_th04288/BaiKt1LeTuanAnhTh04288Application.java
@@ -0,0 +1,13 @@
+package org.example.baikt1_letuananh_th04288;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiKt1LeTuanAnhTh04288Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaiKt1LeTuanAnhTh04288Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/baikt1_letuananh_th04288/BaiKt1LeTuanAnhTh04288ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.baikt1_letuananh_th04288;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaiKt1LeTuanAnhTh04288ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
